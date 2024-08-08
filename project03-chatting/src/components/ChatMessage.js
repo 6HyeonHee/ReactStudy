8a7c32b671758786d4b481cf0a6a0f817f05b05d
heeiu <dbrgusgml@gmail.com>
2024-08-08 18:21:01 +0900

    2024-08-08 project03 완료

--- project03-chatting/src/components/ChatMessage.js
+++ project03-chatting/src/components/ChatMessage.js
@@ -4,6 +4,20 @@ import React, { useEffect, useRef, useState } from 'react';
 import { child, onValue, push, ref, set } from 'firebase/database';
 import { realtime } from '../realtimeConfig';
 
+// 날짜와 시간 포맷 함수
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  };
+  return date.toLocaleDateString(undefined, options);
+};
+
 const scrollTop = (chatWindow) => {
   console.log('scrollTop 호출됨');
   chatWindow.scrollTop = chatWindow.scrollHeight;
@@ -25,10 +39,12 @@ function ChatMessage() {
   function messageWrite(chatRoom, chatId, ChatMessage) {
     // 고유키 생성
     const newPostKey = push(child(ref(realtime), 'tempValue')).key;
+    const timestamp = new Date().toISOString(); // 현재 날짜와 시간을 ISO 문자열 형식으로 저장
     // '방명' 하위에 '고유키'로 구분하여 대화내역을 입력
     set(ref(realtime, chatRoom + '/' + newPostKey), {
       id: chatId,
       message: ChatMessage,
+      timestamp: timestamp,
     });
     console.log('입력성공');
   }
@@ -41,18 +57,39 @@ function ChatMessage() {
       snapshot.forEach((childSnapshot) => {
         // const childKey = childSnapshot.key;
         const childData = childSnapshot.val();
+        const messageDate = formatDate(childData.timestamp);
 
         // console.log("리스너", childKey, childData.id, userId);
         if (childData.id === userId) {
           // 내가 보낸 메세지는 우측으로 정렬
           showDiv.push(
-            <div className="myMsg" style={{ textAlign: 'right' }}>
-              {childData.message}
+            <div className="myMsg Msg" style={{ textAlign: 'right' }}>
+              <div>{userId}</div>
+              {messageDate}
+              <div className="chatting chatme">{childData.message}</div>
+
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                alt="avatar 1"
+                style={{ width: '45px', height: '100%' }}
+              />
             </div>
           );
         } else {
           // 상대방이 보낸 메세지는 좌측으로 정렬
-          showDiv.push(<div>{childData.message}</div>);
+          showDiv.push(
+            <div className="otherMsg Msg">
+              {/* 해당부분을 작성한 아이디로 바꾸고 싶음. */}
+              <div>{childData.id}</div>
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                alt="avatar 1"
+                style={{ width: '45px', height: '100%' }}
+              />
+              <div className="chatting chatother">{childData.message}</div>
+              {messageDate}
+            </div>
+          );
         }
         // 스크롤바를 제일 아래로 내려줌.
         scrollTop(chatWindow.current);
@@ -60,21 +97,20 @@ function ChatMessage() {
       // State를 변경해서 대화내역을 새롭게 렌더링 한다.
       setChatData(showDiv);
     });
-  }, []);
+  }, [chatData]);
 
   return (
     <>
       <div className="App">
-        <h2>Realtime 채팅</h2>
-        대화명 : {userId} &nbsp;&nbsp;
-        <button
-          id="closeBtn"
-          onClick={() => {
-            window.self.close();
-          }}
-        >
-          채팅 종료
-        </button>
+        <div className="header">
+          <h2 style={{ textAlign: 'center' }}>chatRoom</h2>
+          <button
+            id="closeBtn"
+            onClick={() => {
+              window.self.close();
+            }}
+          ></button>
+        </div>
         <div id="chatWindow" ref={chatWindow}>
           {chatData}
         </div>
@@ -102,8 +138,12 @@ function ChatMessage() {
           >
             <input type="hidden" name="chatRoom" value={roomId} />
             <input type="hidden" name="chatId" value={userId} />
-            <input type="text" name="message" />
-            <button type="submit">전송</button>
+            <input
+              type="text"
+              name="message"
+              placeholder="메세지를 입력하세요"
+            />
+            <button className="sendBtn" type="submit"></button>
           </form>
         </div>
       </div>
